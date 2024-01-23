8559199792db7801c15a69e3547a240ad298717b
KiRzEa <107606777+KiRzEa@users.noreply.github.com>
2024-01-23 14:28:42 +0700

    Update train_without_mlm.sh

--- Scripts/train_without_mlm.sh
+++ Scripts/train_without_mlm.sh
@@ -2,125 +2,82 @@
 
 pip install -r requirements.txt --quiet
 
-python train_mlm.py \
-        --model_name "microsoft/mdeberta-v3-base" \
-        --batch_size 128 \
-        --num_train_epochs 10
-
+python train_ce.py \
+        --model_name "FacebookAI/xlm-roberta-large" \
+        --batch_size 16 \
+        --num_train_epochs 10 \
+        --mlm False
+# -------------------------------------------------
 python train_ce.py \
         --model_name "microsoft/mdeberta-v3-base" \
-        --batch_size 64 \
-        --num_train_epochs 10
--------------------------------------------------
-python train_mlm.py \
-        --model_name "bert-base-multilingual-cased" \
-        --batch_size 128 \
-        --num_train_epochs 10
-
+        --batch_size 32 \
+        --num_train_epochs 10 \
+        --mlm False
+# -------------------------------------------------
 python train_ce.py \
         --model_name "bert-base-multilingual-cased" \
-        --batch_size 64 \
-        --num_train_epochs 10
-# -------------------------------------------------
-python train_mlm.py \
-        --model_name "hiiamsid/sentence_similarity_spanish_es" \
-        --batch_size 128 \
+        --batch_size 32 \
         --num_train_epochs 10 \
-        --language "esp"
-
+        --mlm False
+# -------------------------------------------------
 python train_ce.py \
         --model_name "hiiamsid/sentence_similarity_spanish_es" \
-        --batch_size 64 \
+        --batch_size 32 \
         --num_train_epochs 10 \
-        --language "esp"
+        --language "esp" \
+        --mlm False
 # -------------------------------------------------
-python train_mlm.py \
-        --model_name "l3cube-pune/marathi-sentence-similarity-sbert" \
-        --batch_size 128 \
-        --num_train_epochs 10 \
-        --language "mar"
-
 python train_ce.py \
         --model_name "l3cube-pune/marathi-sentence-similarity-sbert" \
-        --batch_size 64 \
+        --batch_size 32 \
         --num_train_epochs 10 \
-        --language "mar"
+        --language "mar" \
+        --mlm False
  # -------------------------------------------------       
-python train_mlm.py \
-        --model_name "l3cube-pune/telugu-sentence-similarity-sbert" \
-        --batch_size 64 \
-        --num_train_epochs 10 \
-        --language "tel"
-
 python train_ce.py \
         --model_name "l3cube-pune/telugu-sentence-similarity-sbert" \
-        --batch_size 64 \
+        --batch_size 32 \
         --num_train_epochs 10 \
-        --language "tel"
+        --language "tel" \
+        --mlm False
 # -------------------------------------------------
-python train_mlm.py \
-        --model_name "mbeukman/xlm-roberta-base-finetuned-kinyarwanda-finetuned-ner-swahili" \
-        --batch_size 128 \
-        --num_train_epochs 10 \
-        --language "kin"
-
 python train_ce.py \
         --model_name "mbeukman/xlm-roberta-base-finetuned-kinyarwanda-finetuned-ner-swahili" \
-        --batch_size 64 \
+        --batch_size 32 \
         --num_train_epochs 10 \
-        --language "kin"
+        --language "kin" \
+        --mlm False
 # -------------------------------------------------
-python train_mlm.py \
-        --model_name "microsoft/deberta-v3-large" \
-        --batch_size 128 \
-        --num_train_epochs 10 \
-        --language "eng"
-
 python train_ce.py \
         --model_name "microsoft/deberta-v3-large" \
-        --batch_size 64 \
+        --batch_size 16 \
         --num_train_epochs 10 \
-        --language "eng"
+        --language "eng" \
+        --mlm False
 # -------------------------------------------------
-python train_mlm.py \
-        --model_name "MoritzLaurer/mDeBERTa-v3-base-xnli-multilingual-nli-2mil7" \
-        --batch_size 128 \
-        --num_train_epochs 10 \
-
 python train_ce.py \
         --model_name "MoritzLaurer/mDeBERTa-v3-base-xnli-multilingual-nli-2mil7" \
-        --batch_size 64 \
+        --batch_size 32 \
         --num_train_epochs 10 \
-# intfloat/multilingual-e5-base
-python train_mlm.py \
-        --model_name "intfloat/multilingual-e5-base" \
-        --batch_size 128 \
-        --num_train_epochs 10 \
-
+        --mlm False
+# -------------------------------------------------
 python train_ce.py \
         --model_name "intfloat/multilingual-e5-base" \
-        --batch_size 64 \
+        --batch_size 32 \
         --num_train_epochs 10 \
-# intfloat/multilingual-e5-base
-python train_mlm.py \
-        --model_name "intfloat/multilingual-e5-large" \
-        --batch_size 128 \
-        --num_train_epochs 10 \
-
+        --mlm False
+# -------------------------------------------------
 python train_ce.py \
         --model_name "intfloat/multilingual-e5-large" \
-        --batch_size 64 \
+        --batch_size 16 \
         --num_train_epochs 10 \
+        --mlm False
 # --------------------------------------------------
-python train_mlm.py \
-        --model_name "intfloat/e5-large-v2" \
-        --batch_size 128 \
-        --num_train_epochs 10 \
-        --language "eng"
-
 python train_ce.py \
         --model_name "intfloat/e5-large-v2" \
-        --batch_size 64 \
+        --batch_size 16 \
         --num_train_epochs 10 \
-        --language "eng"
+        --language "eng" \
+        --mlm False
+        
 zip -r submission.zip submission
